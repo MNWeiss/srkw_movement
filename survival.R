7fb484933d1b6b7112fd14066918bfbe03dea0cd
samellisq <samellisq@gmail.com>
2021-11-08 16:57:50 +0000

    I commited again

--- survival.R
+++ survival.R
@@ -47,7 +47,7 @@ for(i in 1:length(all.ids)){
   
   if(year.out < year.in){
     year = 1986
-    died = 0
+    died = NA
   } else {
     year = seq(year.in, year.out, 1)
     died = rep(0, length(year))
@@ -80,10 +80,96 @@ for(i in 1:length(all.ids)){
   )
 }
 deaths = as_tibble(bind_rows(deaths))
-
+deaths = filter(deaths, !is.na(died)) # these are ids only alive pre the study
 
 #Just for now
 deaths = filter(deaths, sex != 3)
+
 deaths
+
+
+##############################
+#
+###################################
+
+deaths.in = filter(deaths, year >1) # using year before salmon so can't use first year
+
+mat_salmonII = ifelse(is.nan(mat_salmon), 0, mat_salmon)
+
+surv_mod_dat = list(
+  
+  N = nrow(deaths.in),
+  sex = deaths.in$sex,
+  mat = deaths.in$mat,
+  year = deaths.in$year,
+  age_cat = deaths.in$age.cat,
+  died = deaths.in$died,
+  
+  Niter = 200,
+  Nyears = 33,
+  Nmats = 37,
+  mat_salmon = mat_salmonII,
+  mean_salmon = mean_salmon,
+  mat_extant = mat_extant,
+  
+  mean_salmon_seq = seq(quantile(mean_salmon, 0.1), quantile(mean_salmon, 0.9), length.out = 100),
+  mat_salmon_seq = seq(quantile(mat_salmonII, 0.1), quantile(mat_salmonII, 0.9), length.out = 100),
+  mean_salmon_mean = quantile(mean_salmon, 0.1),
+  mean_mat_salmon = mean(mat_salmonII[mat_extant ==1])
+)
+
+
+surv.mod = 
+  stan(file = "survival.stan", data = surv_mod_dat, iter = 500, chains = 4, cores = 4 )
+
+traceplot(surv.mod)
+out = precis(surv.mod, depth = 2)
+out
+
+post = extract(surv.mod)
+
+
+
+meansalmon.plot = 
+  data.frame(
+    mean_salmon = rep(surv_mod_dat$mean_salmon_seq,2),
+    sex = c(rep("F", 100), rep("M",100)),
+    p.mean = c(colMeans(post$post_p_F_meansalmon), colMeans(post$post_p_M_meansalmon)),
+    p.lCI  = c(colQuantiles(post$post_p_F_meansalmon, probs = 0.05), colQuantiles(post$post_p_M_meansalmon, probs = 0.05)),
+    p.uCI  = c(colQuantiles(post$post_p_F_meansalmon, probs = 0.95), colQuantiles(post$post_p_M_meansalmon, probs = 0.95))
+  )
+ggplot(meansalmon.plot, aes(mean_salmon, p.mean, colour = sex, fill = sex))+
+  geom_line(size = 1) +
+  geom_ribbon(aes(ymin = p.lCI, ymax = p.uCI), alpha  = 0.5)
+
+
+matsalmon.plot = 
+  data.frame(
+   mat_salmon = rep(surv_mod_dat$mat_salmon_seq,2),
+    sex = c(rep("F", 100), rep("M",100)),
+    p.mean = c(colMeans(post$post_p_F_matsalmon), colMeans(post$post_p_M_matsalmon)),
+    p.lCI  = c(colQuantiles(post$post_p_F_matsalmon, probs = 0.2), colQuantiles(post$post_p_M_matsalmon, probs = 0.2)),
+    p.uCI  = c(colQuantiles(post$post_p_F_matsalmon, probs = 0.8), colQuantiles(post$post_p_M_matsalmon, probs = 0.8))
+  )
+ggplot(matsalmon.plot, aes(mat_salmon, p.mean, colour = sex, fill = sex))+
+  geom_line(size = 1)+
+  geom_ribbon(aes(ymin = p.lCI, ymax = p.uCI), alpha  = 0.5)
+
+
+#best plot
+ggplot(filter(matsalmon.plot, sex == "F"), aes(mat_salmon, p.mean, colour = sex, fill = sex))+
+  geom_line(size = 2)+
+  geom_ribbon(aes(ymin = p.lCI, ymax = p.uCI), alpha  = 0.5, colour = NA) +
+  xlab(" 'Salmon Achieved' ")+
+  ylab("Probability of Dying") +
+  scale_colour_manual(values = "firebrick4")+
+  scale_fill_manual(values = "firebrick4")+
+  theme_bw() +
+  theme(
+    axis.text = element_text(size = 20),
+    axis.title = element_text(size = 30),
+    legend.position = "none"
+  )
+
 
 
--- survival.stan
+++ survival.stan
@@ -1,3 +1,71 @@
 data {
+  int N;
+  int sex[N];
+  int mat[N];
+  int year[N];
+  int age_cat[N];
+  int died[N];
   
+  int Niter;
+  int Nyears;
+  int Nmats;
+  real mat_salmon[Niter, Nyears, Nmats];
+  real mean_salmon[Nyears, Niter];
+  int mat_extant[Nyears, Nmats];
+  
+  vector[100] mean_salmon_seq;
+  vector[100] mat_salmon_seq;
+  real mean_salmon_mean;
+  real mean_mat_salmon;
+  
+}
+
+parameters{
+  
+  // real<lower = 0.25, upper = 0.75> B0[2];
+  real Bage[8];
+  real Bmu_salmon[1];
+  real Bmat_salmon[1];
+  
+}
+
+model{
+  vector[N] p;
+  
+  //priors
+  // B0 ~ normal(0.5,5);
+  Bage ~ normal(-3.38,0.05);
+  Bmu_salmon ~ normal(0,5);
+  Bmat_salmon ~ normal(0,5);
+  
+  for(i in 1:N){
+    real p_i;
+    if(mat_extant[year[i], mat[i]]){
+          p_i = Bage[age_cat[i]] + (Bmu_salmon[1]*mean_salmon[year[i]-1,1])+(Bmat_salmon[1]*mat_salmon[1,year[i]-1,mat[i]]);
+          p[i] = inv_logit(p_i);
+    }
+
+  }
+  
+  died ~ bernoulli(p);
+}
+
+generated quantities{
+
+  vector[100] post_p_F_meansalmon;
+  vector[100] post_p_M_meansalmon;
+  
+  vector[100] post_p_F_matsalmon;
+  vector[100] post_p_M_matsalmon;
+
+  for(i in 1:100){
+    post_p_F_meansalmon[i] = inv_logit(Bage[4]  + (Bmu_salmon[1]*mean_salmon_seq[i])+(Bmat_salmon[1]*mean_mat_salmon));
+    post_p_M_meansalmon[i] = inv_logit(Bage[8]  + (Bmu_salmon[1]*mean_salmon_seq[i])+(Bmat_salmon[1]*mean_mat_salmon));
+    
+    post_p_F_matsalmon[i] = inv_logit(Bage[4]  + (Bmu_salmon[1]*mean_salmon_mean)+(Bmat_salmon[1]*mat_salmon_seq[i]));
+    post_p_M_matsalmon[i] = inv_logit(Bage[8]  + (Bmu_salmon[1]*mean_salmon_mean)+(Bmat_salmon[1]*mat_salmon_seq[i]));
+  }
+
+
+
 }
